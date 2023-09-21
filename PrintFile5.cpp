de81815fbc4f7959fad3533a38e2e9beaff07ceb
EvGeniy0505 <142892547+EvGeniy0505@users.noreply.github.com>
2023-09-21 17:54:24 +0300

    Add files via upload

--- PrintFile5.cpp
+++ PrintFile5.cpp
@@ -17,6 +17,8 @@ void PrintText(char **ArrOfPtr, int QuantityOfStrs);
 
 size_t LenFile(FILE * fp);
 
+int compare(const void *str1, const void *str2);
+
 
 
 
@@ -44,26 +46,55 @@ int main()
 
     for(NumOfStr = 1; NumOfStr < SizeOfFile; NumOfStr++)
     {
-        if (Buff[NumOfStr] == '\0')
+        if ((Buff[NumOfStr] == '\0') && ((NumOfStr) != (SizeOfFile-1)))
         {
             ArrOfPtr[CntOfPtr] = &(Buff[NumOfStr+1]);
             CntOfPtr++;
         }
     }
 
+    /*int Sravn = 0;
+    char * Swap = 0;
 
+    for(NumOfStr = 0; NumOfStr < QuantityOfStrs - 1; NumOfStr++)
+    {
+        for(size_t NumOfStr_ = 0; NumOfStr_ < QuantityOfStrs - 1; NumOfStr_++)
+        {
+            Sravn = strcmp(ArrOfPtr[NumOfStr], ArrOfPtr[NumOfStr_]);
+            if (Sravn < 0)
+            {
+                Swap = ArrOfPtr[NumOfStr_];
+                ArrOfPtr[NumOfStr_] = ArrOfPtr[NumOfStr];
+                ArrOfPtr[NumOfStr] = Swap;
+            }
+        }
+    }      */
+
+
+
+    qsort(ArrOfPtr, QuantityOfStrs, sizeof(char*), compare);
 
     PrintText(ArrOfPtr, QuantityOfStrs);
+
 
     free(Buff);
     free(ArrOfPtr);
 
     fclose(fp);
 
+    return 0;
+
 
 }
 
 
+
+int compare(const void *str1, const void *str2)
+    {
+        char * str3 = *(char**)str1;
+        char * str4 = *(char**)str2;
+        return strcmp(str3, str4);
+    }
 
 void PrintText(char **ArrOfPtr, int QuantityOfStrs)
 {

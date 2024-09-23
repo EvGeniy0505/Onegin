8e7e889c8f192269d6be1d948bf11d66c5687379
Evgeniy Rogov <rogoff050505@gmail.com>
2024-09-24 01:51:52 +0300

    Begin qsort

---
+++ quick_sort.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int COMPARE(const void* a, const void* b);
+
+void my_qsort(void* sort_system, size_t elements, size_t size, int COMPARE(const void* a, const void* b));
+
+int main()
+{
+    int len_arr = 8;
+
+    int arr[len_arr] = {25, 52, 7, 10, 57, 1, 39, -3};
+
+
+
+    return 0;
+}
+
+int COMPARE(const void* a, const void* b)
+{
+
+}
+
+void my_qsort(void* sort_system, size_t elements, size_t size, int COMPARE(const void* a, const void* b))
+{
+
+}

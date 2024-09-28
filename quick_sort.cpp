ceee983746c6b22c0ea98773aad1df0e4d7413f6
Evgeniy Rogov <rogoff050505@gmail.com>
2024-09-28 16:25:10 +0300

    Finish qsort for int

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int COMPARE_INT(const int* a, const int* b);
+int COMPARE_INT(int a, int b);
 
-void my_qsort(int* sort_system, size_t elements, size_t size, int COMPARE(const int* a, const int* b));
+void my_qsort(int* sort_system, size_t elements, size_t size, int COMPARE( int a,  int b));
 
 int main()
 {
@@ -22,33 +22,35 @@ int main()
     return 0;
 }
 
-int COMPARE_INT(const int* a, const int* b)
+int COMPARE_INT(int a, int b)
 {
-    const int real_a = *(const int*)a;
-    const int real_b = *(const int*)b;
+    // const int real_a = *a;
+    // const int real_b = *(const int*)b;
 
-    return real_a - real_b;
+    return a - b;
 }
 
-void my_qsort(int* sort_system, size_t elements, size_t size, int COMPARE(const int* a, const int* b))
+void my_qsort(int* sort_system, size_t elements, size_t size, int COMPARE( int a,  int b))
 {
-    size_t half_symb = elements / 2;
+    int half_symb = sort_system[elements / 2];
+
+    printf("%d %zu\n", half_symb, elements / 2);
     
     int ptr_1 = 0;
     int ptr_2 = elements - 1;
 
     do
     {
-        while(COMPARE(&sort_system[ptr_1], &sort_system[half_symb]) < 0)
+        while(COMPARE(sort_system[ptr_1], half_symb) < 0)
         {
             ptr_1++;
         }
-        while(COMPARE(&sort_system[ptr_2], &sort_system[half_symb]) > 0)
+        while(COMPARE(sort_system[ptr_2], half_symb) > 0)
         {
             ptr_2--;
         }
 
-        printf("%d %d\n", ptr_1, ptr_2);
+        // printf("%d %d\n", ptr_1, ptr_2);
 
         if(ptr_1 <= ptr_2)
         {
@@ -63,19 +65,12 @@ void my_qsort(int* sort_system, size_t elements, size_t size, int COMPARE(const 
 
     }while(ptr_1 <= ptr_2);
 
-    for(size_t i = 0; i < elements; i++)
-    {
-        printf("%d ", sort_system[i]);
-    }
-    putchar('\n');
-    printf("%d %d\n", ptr_1, ptr_2);
-
     if(ptr_2 > 0)
     {
         my_qsort(sort_system, ptr_2 + 1 , sizeof(*sort_system), COMPARE_INT);
     }
-    if(ptr_1 < elements - 1)
+    if(ptr_1 < elements)
     {
-        my_qsort(&sort_system[ptr_1], elements - ptr_1, sizeof(*sort_system), COMPARE_INT);
+        my_qsort(sort_system + ptr_1, elements - ptr_1, sizeof(*sort_system), COMPARE_INT);
     }
 }
